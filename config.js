3834d376d3ddd6bf1aefa0cc5366a9ad1359d7cf
James Lucas <jamesmlucas@gmail.com>
2016-10-12 14:48:59 -0500

    change default port

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-config.port = process.env.PORT || 80;
+config.port = process.env.PORT || 8080;
 config.mongoUrl = process.env.MONGO_URL || null;
 config.collection = process.env.COLLECTION || null;
 
